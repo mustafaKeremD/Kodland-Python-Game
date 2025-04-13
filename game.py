10b6e8056b469efefb2524a9df634f24ba9e7c21
mustafaKeremD <165467473+mustafaKeremD@users.noreply.github.com>
2025-04-13 18:16:56 +0300

    last version

--- game.py
+++ game.py
@@ -7,21 +7,21 @@ WIDTH = 816
 HEIGHT = 624
 TOTAL_ENEMY_NUMBER = 6
 
-# Global variables which needed in more than one function across the code
+# Global variables needed in more than one function across the code
 game_state = "menu"
 music_state = "on"
 buttons = {
-            "Start": Rect((298, 270), (200, 50)),
-            "Music on/off": Rect((298, 370), (200, 50)),
-            "Exit": Rect((298, 470), (200, 50)),
-          }
+    "Start": Rect((298, 270), (200, 50)),
+    "Music on/off": Rect((298, 370), (200, 50)),
+    "Exit": Rect((298, 470), (200, 50)),
+}
 
 
-enemy_entry_positions = [(0, HEIGHT/2), (WIDTH/2, 0), (WIDTH/2, HEIGHT), (WIDTH, HEIGHT/2)]
+enemy_entry_positions = [(0, HEIGHT / 2), (WIDTH / 2, 0),
+                         (WIDTH / 2, HEIGHT), (WIDTH, HEIGHT / 2)]
 enemy_types = ["flame", "bamboo"]
 enemies = []
 enemy_count = 0
-
 
 is_attacking = False
 attack_frame = 0
@@ -40,7 +40,8 @@ sounds.theme.play(-1)
 class Player:
 
     def __init__(self):
-        self.actor = Actor("character/idle/down/frame_0", (WIDTH/2, HEIGHT/2))
+        self.actor = Actor("character/idle/down/frame_0",
+                           (WIDTH / 2, HEIGHT / 2))
 
         self.state = "idle"
 
@@ -60,28 +61,28 @@ class Player:
 
             self.change_state("run")
             self.direction = "left"
-            self.actor.left -=4
+            self.actor.left -= 4
 
         elif keyboard.right and self.actor.right < 784:
             self.change_state("run")
             self.direction = "right"
-            self.actor.right +=4
+            self.actor.right += 4
 
         elif keyboard.up and self.actor.top > 32:
             self.change_state("run")
             self.direction = "up"
-            self.actor.top -=4
+            self.actor.top -= 4
 
         elif keyboard.down and self.actor.bottom < 594:
             self.change_state("run")
             self.direction = "down"
-            self.actor.bottom +=4
+            self.actor.bottom += 4
 
         else:
             self.change_state("idle")
 
         if keyboard.space and not is_attacking:
-            self.attack_pos = check_hit(p1, enemies)
+            self.attack_pos = check_hit(player, enemies)
             sounds.slash.play()
 
             is_attacking = True
@@ -90,7 +91,7 @@ class Player:
 
     def animate(self, current_frame):
 
-       self.actor.image = f"character/{self.state}/{self.direction}/frame_{current_frame}"
+        self.actor.image = f"character/{self.state}/{self.direction}/frame_{current_frame}"
 
 
 class Enemy:
@@ -117,12 +118,14 @@ class Enemy:
 def check_collision(player, enemy_list):
     global monsters_left
 
+    #Check for if player actor collides with any enemy actor
     for i in range(len(enemy_list)):
         if (player.actor.colliderect(enemy_list[i].actor)
                 and enemy_list[i].state == 0):
-           player.lives -=1
-           enemy_list[i].state = 1
-           monsters_left -=1
+            player.lives -= 1
+            enemy_list[i].state = 1
+            monsters_left -= 1
+            sounds.damage.play()
 
 
 def check_hit(player, enemy_list):
@@ -132,16 +135,16 @@ def check_hit(player, enemy_list):
     hit_range = 32
     x, y = player.actor.center
 
-    if p1.direction == "down":
+    if player.direction == "down":
         hit_point = (x, y + hit_range)
 
-    if p1.direction == "right":
+    if player.direction == "right":
         hit_point = (x + hit_range, y)
 
-    if p1.direction == "up":
+    if player.direction == "up":
         hit_point = (x, y - hit_range)
 
-    if p1.direction == "left":
+    if player.direction == "left":
         hit_point = (x - hit_range, y)
 
     for i in range(len(enemy_list)):
@@ -162,13 +165,19 @@ def change_game_state(new_state):
 
 def spawn_enemy():
     global enemy_count
+    #Appends a member of Enemy class with randomized type and spawn point
+    #There is 4 possible spawn point declared in enemy_entry_positions
     if enemy_count < TOTAL_ENEMY_NUMBER:
-        enemies.append(Enemy(random.choice(enemy_types), random.choice(enemy_entry_positions) , 2))
+        enemies.append(
+            Enemy(
+                random.choice(enemy_types),
+                random.choice(enemy_entry_positions),
+                2))
         enemy_count += 1
         clock.schedule_unique(spawn_enemy, random.randint(1, 5))
 
 
-p1 = Player()
+player = Player()
 
 
 def update(dt):
@@ -180,27 +189,32 @@ def update(dt):
         if frame_timer >= frame_delay:
             frame_timer = 0
             player_current_frame = (player_current_frame + 1) % 4
-            p1.animate(player_current_frame)
+            player.animate(player_current_frame)
 
             for i in range(len(enemies)):
                 enemies[i].animate(player_current_frame)
 
         random.seed(player_current_frame)
-        p1.move()
+        player.move()
 
         for i in range(len(enemies)):
-            enemies[i].move(p1.actor.x, p1.actor.y)
+            enemies[i].move(player.actor.x, player.actor.y)
 
-        check_collision(p1, enemies)
+        check_collision(player, enemies)
 
-        if p1.lives == 0:
+        if player.lives == 0:
             draw_game_screen()
+            sounds.theme.stop()
+            sounds.game_over.play()
             game_state = "lose"
 
-        if monsters_left == 0 and p1.lives != 0:
+        if monsters_left == 0 and player.lives != 0:
             draw_game_screen()
+            sounds.theme.stop()
+            sounds.win.play()
             game_state = "win"
 
+        #Calculate and animate attack animation only 
         if is_attacking:
             attack_timer += dt
             if attack_timer >= attack_frame_duration:
@@ -215,13 +229,13 @@ def draw_game_screen():
 
     screen.clear()
     screen.blit('map', (0, 0))
-    p1.actor.draw()
+    player.actor.draw()
 
     for i in range(len(enemies)):
         if enemies[i].state == 0:
             enemies[i].actor.draw()
 
-    screen.draw.text(f"Lives : {p1.lives}", (50, 70), fontsize=50)
+    screen.draw.text(f"Lives : {player.lives}", (50, 70), fontsize=50)
     screen.draw.text(
         f"Monsters left : {monsters_left}", (500, 70), fontsize=50)
 
@@ -229,22 +243,19 @@ def draw_game_screen():
 
         screen.blit(
             attack_frames[attack_frame],
-            (p1.attack_pos[0] - 32,
-             p1.attack_pos[1] - 32))
+            (player.attack_pos[0] - 32,
+             player.attack_pos[1] - 32))
 
 
 def draw_menu_screen():
     screen.fill((27, 166, 154))
-
     screen.draw.text("THE GAME", center=(WIDTH // 2, 100), fontsize=80)
-
     screen.draw.text(
         "\"Arrow keys to move\"",
         center=(
             WIDTH // 2,
             180),
         fontsize=50)
-    
     screen.draw.text(
         "\"Space to attack\"",
         center=(
@@ -258,10 +269,11 @@ def draw_menu_screen():
 
 
 def lose_screen():
-    screen.draw.text("GAME OVER", center=(WIDTH // 2, HEIGHT/2), fontsize=80)
+    screen.draw.text("GAME OVER", center=(WIDTH // 2, HEIGHT / 2), fontsize=80)
 
 
 def draw():
+
 
     if game_state == "menu":
         draw_menu_screen()
